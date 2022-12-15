54ad41dcc7d60c87b22723d077f08892976a2663
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-15 15:09:50 +0100

    added search menu

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 const languageFlag = document.querySelector(".top-flag");
+const searchIcon = document.querySelector(".top-search");
 
 let currentLanguage = "swedish";
+
+searchIcon.addEventListener("click", () => {
+  console.log("search click");
+  const div = document.createElement("div");
+  div.className = "top-search-screen";
+  div.innerHTML = `
+  <div class="top-search-screen">
+    <img
+        class="search-screen-background"
+        src="assets/images/icons/scroll.png" />
+    <h2 class="search-screen-title">Sök och filtrera</h2>
+    <div class="search-screen-filters">
+        <p class="filters-options">
+            <input class="filters-checkbox" type="checkbox" />
+            <span>Glutenfritt</span>
+        </p>
+        <p class="filters-options">
+            <input class="filters-checkbox" type="checkbox" />
+            <span>Laktosfritt</span>
+        </p>
+        <p class="filters-options">
+            <input class="filters-checkbox" type="checkbox" />
+            <span>Äggfritt</span>
+        </p>
+        <p class="filters-options">
+            <input class="filters-checkbox" type="checkbox" />
+            <span>Soja</span>
+        </p>
+    </div>
+    <div class="search-screen-user-input">
+        <input class="user-input-input" />
+        <button class="user-input-search-btn">Sök</button>
+        </div>
+    </div>
+  `;
+  document.querySelector(".page-header").append(div);
+  div.querySelector(".user-input-search-btn").addEventListener("click", () => {
+    document.querySelector(".top-search-screen").remove();
+  });
+});
 
 languageFlag.addEventListener("click", () => {
   const languageDiv = document.createElement("div");
