d2699c31b3f1f6368417d384d768d23a6de229a7
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-15 11:49:34 +0100

    language can now be changed

---
+++ app.js
@@ -0,0 +1,35 @@
+const languageFlag = document.querySelector(".top-flag");
+
+let currentLanguage = "swedish";
+
+languageFlag.addEventListener("click", () => {
+  const languageDiv = document.createElement("div");
+  languageDiv.className = "header-lang-select";
+  languageDiv.innerHTML = `
+  <h2 class="lang-select-title">Select your language</h2>
+    <div class="lang-select-flags">
+        <img class="flag-img" src="assets/images/icons/swedish-flag.png" />
+        <img class="flag-img" src="assets/images/icons/english-flag.png" />
+    </div>
+    <img class="lang-select-background" src="assets/images/icons/scroll.png" />
+  `;
+  setLanguageFunctions(languageDiv);
+  document.querySelector(".page-header").append(languageDiv);
+});
+
+function setLanguageFunctions(container) {
+  const flags = container.querySelectorAll(".flag-img");
+  flags[0].addEventListener("click", () => {
+    setLanguage("swedish");
+    container.remove();
+  });
+  flags[1].addEventListener("click", () => {
+    setLanguage("english");
+    container.remove();
+  });
+}
+
+function setLanguage(language) {
+  currentLanguage = language;
+  languageFlag.setAttribute("src", `assets/images/icons/${language}-flag.png`);
+}
